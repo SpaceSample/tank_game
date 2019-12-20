c5f4a7c0e3e56d496984208ccabe14a742ced148
SpaceSample <wwjjxx0312@126.com>
2019-12-21 00:47:56 +0800

    Make game run.

--- src/game/game.js
+++ src/game/game.js
@@ -1,19 +1,24 @@
-import { Application } from '@pixi/app';
-import { AppLoaderPlugin } from '@pixi/loaders';
-import * as utils from '@pixi/utils';
-import { Sprite } from '@pixi/sprite';
-
-// class Game {
-//     construcor(container, width, height){
-//         this.app = new PIXI.Application({width, height});
-//         container.appendChild(app.view);
-//     }
-// }
-Application.registerPlugin(AppLoaderPlugin);
-const app = new Application();
-app.loader.add('assets/xl.jpg');
+import * as PIXI from 'pixi.js'
+PIXI.Application.registerPlugin(PIXI.AppLoaderPlugin);
+const app = new PIXI.Application({width: 800, height: 600});
+app.loader.add('assets/tank1.png');
+const ticker = new PIXI.Ticker();
 app.loader.load(() => {
-    const sprite = new Sprite(utils.TextureCache['assets/xl.jpg']);
-    app.stage.addChild(sprite);
+    console.log(app.renderer);
+    const tank1 = new PIXI.Sprite(PIXI.utils.TextureCache['assets/tank1.png']);
+    const container = new PIXI.Container();
+    app.stage.addChild(container);
+    const texture = PIXI.Texture.from('assets/xl.jpg');
+    const sp = new PIXI.Sprite(texture);
+    container.addChild(sp);
+    container.addChild(tank1);
+    app.ticker.add(d => {
+        sp.x +=10;
+        if(sp.x>700){
+            sp.x = 0;
+        }
+    });
+   ticker.start();
 });
+
 export default app;
