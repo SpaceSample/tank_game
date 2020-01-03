40db07d8a02945153018c00c7d2db4c4c20da2df
SpaceSample <wwjjxx0312@126.com>
2020-01-03 23:23:21 +0800

    Add circle collision check

--- src/game/util/collision_detection.js
+++ src/game/util/collision_detection.js
@@ -11,6 +11,26 @@ function getBox(sp){
   };
 }
 
+function getMaxRadius(sp){
+  if (sp.maxRadius ===undefined){
+    const b = getBox(sp);
+    const r1 = Math.sqrt(b.x1*b.x1 + b.y1*b.y1);
+    const r2 = Math.sqrt(b.x2*b.x2 + b.y1*b.y1);
+    const r3 = Math.sqrt(b.x1*b.x1 + b.y2*b.y2);
+    const r4 = Math.sqrt(b.x2*b.x2 + b.y2*b.y2);
+    sp.maxRadius = Math.max(r1, r2, r3, r4);
+  }
+  return sp.maxRadius;
+}
+
+function checkCircle(sp1, sp2){
+  const d = getMaxRadius(sp1) + getMaxRadius(sp2);
+  const d2 = d*d;
+  const dx = sp1.x-sp2.x;
+  const dy = sp1.y-sp2.y;
+  return (dx*dx + dy*dy) <= d2;
+}
+
 function checkPoint(p, sp){
   const b = getBox(sp);
   const lp = sp.toLocal(p);
@@ -19,6 +39,9 @@ function checkPoint(p, sp){
 }
 
 function check4Point(sp1, sp2){
+  if (!checkCircle(sp1, sp2)){
+    return false;
+  }
   const b = getBox(sp1);
   const p1 = sp1.toGlobal(new PIXI.Point(b.x1, b.y1));
   if(checkPoint(p1, sp2)){
@@ -48,6 +71,9 @@ function check4PointWithMany(sp1, sps){
 
   for(let i in sps){
     const sp2 = sps[i];
+    if (!checkCircle(sp1, sp2)){
+      continue;
+    }
     if(checkPoint(p1, sp2)){
       return sp2;
     }
