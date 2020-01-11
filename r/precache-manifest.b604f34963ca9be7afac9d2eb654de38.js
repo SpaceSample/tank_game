4c8361fb8ca34778481974b3bc5ee8ab3151141a
SpaceSample <wwjjxx0312@126.com>
2020-01-11 11:30:45 +0800

    Update build

---
+++ r/precache-manifest.b604f34963ca9be7afac9d2eb654de38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf4cc8034382541af1d117400625b18",
+    "url": "/tank_game/r/index.html"
+  },
+  {
+    "revision": "3efeb77c784435f4adcd",
+    "url": "/tank_game/r/static/js/2.cb0053f5.chunk.js"
+  },
+  {
+    "revision": "0e3d074a0fbcb9b1fc90efb09bcbe52f",
+    "url": "/tank_game/r/static/js/2.cb0053f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d95edc99cf4c8544ee32",
+    "url": "/tank_game/r/static/js/main.a176c1ea.chunk.js"
+  },
+  {
+    "revision": "524019669bf58c90907e",
+    "url": "/tank_game/r/static/js/runtime-main.e7e5dec5.js"
+  }
+]);
